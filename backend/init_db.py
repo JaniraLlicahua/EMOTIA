5205be19dc38ee540a42377beda4eaff334ccdb5
Janira <U22214187@utp.edu.pe>
2025-12-03 05:25:28 -0500

    Limpiando repositorio para nueva estructura

--- backend/init_db.py
+++
@@ -1,7 +0,0 @@
-#backend\init_db.py
-from .database import Base, engine
-from . import models 
-
-print("🧱 Creando tablas en la base de datos...")
-Base.metadata.create_all(bind=engine)
-print("✅ Tablas creadas correctamente.")
--- backend/utils/security.py
+++
@@ -1,9 +0,0 @@
-# backend/utils/security.py
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def get_password_hash(password: str):
-    if len(password) > 72:
-        password = password[:72]
-    return pwd_context.hash(password)
